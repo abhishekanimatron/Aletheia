9898d2028828fe270e5e26a6f46fa2e15746cdeb
abhishekanimatron <abhishekverma297ab@gmail.com>
2024-02-11 23:28:40 +0530

    making main content react to sidebar collapse

---
+++ app/(browse)/_components/container.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { cn } from "@/lib/utils"
+import { useSidebar } from "@/store/use_sidebar"
+import { useEffect } from "react"
+import { useMediaQuery } from "usehooks-ts"
+
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+export const Container = ({
+    children
+}: ContainerProps) => {
+    const matches = useMediaQuery("(max-width:1024px)");
+    const { collapsed, onCollapse, onExpand } = useSidebar((state) => state)
+    useEffect(() => {
+        //matches:true if we are in mobile mode
+        if (matches) {
+            onCollapse()
+        }
+        else {
+            onExpand()
+        }
+        //what this does is, it automatically expands and collapses
+        // the sidebar based on size
+    }, [matches, onCollapse, onExpand])
+
+    return (
+        <div className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}>
+            {children}
+        </div>
+    )
+}
